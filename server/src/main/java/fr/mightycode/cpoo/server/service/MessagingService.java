96eb8ca804c9c2cca8397218ce91234bc3b0e4a4
vbarat <vincent.barat@gmail.com>
2023-08-08 14:51:07 +0200

    Better implementation of WebSocket reconnection

--- server/src/main/java/fr/mightycode/cpoo/server/service/MessagingService.java
+++ server/src/main/java/fr/mightycode/cpoo/server/service/MessagingService.java
@@ -3,7 +3,6 @@ package fr.mightycode.cpoo.server.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
@@ -13,21 +12,10 @@ public class MessagingService {
 
     private static final Logger logger = LoggerFactory.getLogger(MessagingService.class);
 
-    private final RouterStompSessionHandler routerStompSessionHandler;
-
-    private final WebSocketStompClient webSocketStompClient;
-
     MessagingService() {
-        routerStompSessionHandler = new RouterStompSessionHandler();
-        webSocketStompClient = new WebSocketStompClient(new StandardWebSocketClient());
+        WebSocketStompClient webSocketStompClient = new WebSocketStompClient(new StandardWebSocketClient());
         webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        RouterStompSessionHandler routerStompSessionHandler = new RouterStompSessionHandler(webSocketStompClient);
         webSocketStompClient.connectAsync("ws://localhost:8080/router", routerStompSessionHandler);
-    }
-
-    @Scheduled(cron = "* * * * * ?")
-    void reconnect() {
-        logger.info("### reconnect");
-        if (!routerStompSessionHandler.getStompSession().isConnected())
-            webSocketStompClient.connectAsync("ws://localhost:8080/router", routerStompSessionHandler);
     }
 }
--- server/src/main/java/fr/mightycode/cpoo/server/service/RouterStompSessionHandler.java
+++ server/src/main/java/fr/mightycode/cpoo/server/service/RouterStompSessionHandler.java
@@ -1,7 +1,7 @@
 package fr.mightycode.cpoo.server.service;
 
 import fr.mightycode.cpoo.server.model.Message;
-import lombok.Getter;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -10,17 +10,19 @@ import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 import java.lang.reflect.Type;
 
-@Getter
-@Component
 public class RouterStompSessionHandler extends StompSessionHandlerAdapter {
 
     private static final Logger logger = LoggerFactory.getLogger(RouterStompSessionHandler.class);
 
-    private StompSession stompSession = null;
+    private final WebSocketStompClient webSocketStompClient;
+
+    RouterStompSessionHandler(WebSocketStompClient webSocketStompClient) {
+        this.webSocketStompClient = webSocketStompClient;
+    }
 
     @Override
     public Type getPayloadType(StompHeaders headers) {
@@ -32,7 +34,6 @@ public class RouterStompSessionHandler extends StompSessionHandlerAdapter {
     @Override
     public void afterConnected(StompSession session, StompHeaders headers) {
         logger.info("Client connected: headers {}", headers);
-        stompSession = session;
         session.subscribe("/domain/acme/incomingMessages", this);
     }
 
@@ -46,8 +47,14 @@ public class RouterStompSessionHandler extends StompSessionHandlerAdapter {
         logger.error("Client error: exception {}, command {}, payload {}, headers {}", exception.getMessage(), command, payload, headers);
     }
 
+    @SneakyThrows
     @Override
     public void handleTransportError(StompSession session, Throwable exception) {
         logger.error("Client transport error: error {}", exception.getMessage());
+        if (!session.isConnected()) {
+            Thread.sleep(1000);
+            logger.info("Trying to reconnect...");
+            webSocketStompClient.connectAsync("ws://localhost:8080/router", this);
+        }
     }
 }
