dbbd82e5fd4c52ca9459fd21cafee3316b0221bc
Leo.Lessirard <leo.lessirard@insa-rennes.fr>
2023-09-12 23:39:10 +0200

    Element placement and mouse behavior + page routing changes

--- client/front/src/app/app-routing.module.ts
+++ client/front/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './components/login/create-account/create-account.component';
+import { ForgotPasswordComponent } from './components/login/forgot-password/forgot-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige vers /login par défaut
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'settings', component: SettingsComponent},
+  { path: 'forgot-password', component: ForgotPasswordComponent},
+  { path: 'create-account', component: CreateAccountComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/front/src/app/components/settings/settings.component.ts
+++ client/front/src/app/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -7,20 +8,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class SettingsComponent {
 
-  /*
-    Cette zone permet de réaliser le changement de page, lors qu'une des fonctions est appellé (après un click sur un bouton), elle va permettre l'envoie
-    d'une message au père (<app.component>) qui va activé la bonne fonction pour changer la page.
-  */
-  @Output() goLogin = new EventEmitter<void>();
-  @Output() goHome = new EventEmitter<void>();
-
+  constructor(private router:Router){}
 
   settingsToHome() : void{
-    this.goHome.emit();
+    this.router.navigate(['/home']);
   }
 
-  settingsToLogin() : void{    
-    this.goLogin.emit();
+  settingsToLogin() : void{  
+    this.router.navigate(['/login'])  
   }
 
   /* ----------------------------------------------------------------------------------------------------------------------------------------- */
