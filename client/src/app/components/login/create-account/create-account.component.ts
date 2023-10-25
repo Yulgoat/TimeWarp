62d2b1592bf1c3a210a2b391c61f53d9958b9365
Pierre <pierre.ouvrard@insa-rennes.fr>
2023-10-25 19:17:13 +0200

    Upgrade of the fields of SignUp Front, seconde version #65

--- client/src/app/components/login/create-account/create-account.component.ts
+++ client/src/app/components/login/create-account/create-account.component.ts
@@ -17,15 +17,16 @@ export class CreateAccountComponent {
 
   UserDTO : JSON = <JSON><unknown>{}            // Json which contains the information that will be sent to the server
 
-  userOK = true;
-  emailOK = true;
-  pwdOk = true;
-  CfmPwdOK = true;
+  userErrorMessage : string ="";
+  emailErrorMessage : string ="";
+  pwdErrorMessage : string ="";
+  cfpwdErrorMessage : string ="";
 
-  EmptyUser = false;
-  EmptyEmail = false;
-  EmptyPwd = false;
-  EmptyCfmPwd = false;
+  userError : boolean = false;
+  emailError : boolean = false;
+  pwdError : boolean = false;
+  cfpwdError : boolean = false;
+
 
 
   /* Router To Login*/
@@ -35,66 +36,65 @@ export class CreateAccountComponent {
 
   /* Verify that password and confirmPwd are identical */
   samePwd():boolean{
-    return this.pwdOk=this.CfmPwdOK=(this.password===this.confirmPwd);
+    return (this.password===this.confirmPwd);
   }
 
   /* Using a regex we check if what the user enters in email looks like an email address */
   validation_email() : boolean{
     let sampleRegEx: RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return this.emailOK=(sampleRegEx.test(this.email));
+    return sampleRegEx.test(this.email);
   }
 
   username_empty (): boolean{
-    if(this.username==="") {this.EmptyUser = true; this.userOK = false; return true;}
-    else{this.EmptyUser = false; this.userOK = true; return false;}
+    if(this.username==="") {this.userErrorMessage = "Field Empty";return true; }
+    else{return false;}
   }
   email_empty (): boolean{
-    if(this.email==="") {this.EmptyEmail = true; this.emailOK = false; return true;}
-    else{this.EmptyEmail = false; this.emailOK = true; return false;}
+    if(this.email==="") {this.emailErrorMessage = "Field Empty";return true; }
+    else{return false;}
   }
   pwd_empty (): boolean{
-    if(this.password==="") {this.EmptyPwd = true; this.pwdOk = false; return true;}
-    else{this.EmptyPwd = false; this.pwdOk = true; return false;}
+    if(this.password==="") {this.pwdErrorMessage = "Field Empty";return true; }
+    else{return false;}
   }
   cfmPwd_empty (): boolean{
-    if(this.confirmPwd==="") {this.EmptyCfmPwd = true; this.CfmPwdOK = false; return true;}
-    else{this.EmptyCfmPwd = false; this.CfmPwdOK = true; return false;}
+    if(this.confirmPwd==="") {this.cfpwdErrorMessage = "Field Empty";return true; }
+    else{return false;}
   }
 
 
   /* Main Fonction */
   createAccountFonction() : void{
-    let isUsernameEmpty = this.username_empty();
-    let isEmailEmpty = this.email_empty();
-    let isPwdEmpty = this.pwd_empty();
-    let isCfmPwdEmpty = this.cfmPwd_empty();
-    
-    if(!isUsernameEmpty||!isEmailEmpty||!isPwdEmpty||!isCfmPwdEmpty){
-      let isPasswordValid = this.samePwd();
-      let isEmailValid = this.validation_email();
-  
-      if (isPasswordValid && isEmailValid) {
-          this.UserDTO = <JSON><unknown>{
-            "username": this.username,
-            "email" : this.email,
-            "password": this.password
-          }          
-          console.log(this.UserDTO);
-          this.navigateToLogin();
-      }
-      else{
-        console.log("Email or password problem");
-      }
-    }  
+    if (this.username_empty()) this.userError = true; else this.userError = false;
+    if (this.email_empty()) this.emailError = true; else this.emailError = false;
+    if (this.pwd_empty()) this.pwdError = true; else this.pwdError = false;
+    if (this.cfmPwd_empty()) this.cfpwdError = true; else this.cfpwdError = false;
 
-    if (!this.userOK) 
-      this.username = ''; // Réinitialisez la valeur du champ username    
-    if (!this.emailOK)
-      this.email = ''; // Réinitialisez la valeur du champ email     
-    if (!this.pwdOk) 
-      this.password = ''; // Réinitialisez la valeur du champ password    
-    if (!this.CfmPwdOK) 
-      this.confirmPwd = ''; // Réinitialisez la valeur du champ confirmPwd  
+    let isSamePassword = this.samePwd();
+    let isCorrectEmail = this.validation_email();
+
+    if(!this.pwdError && !isSamePassword){ this.pwdError = true; this.pwdErrorMessage = "Not the Same Password";}
+    if(!this.cfpwdError && !isSamePassword){this.cfpwdError = true; this.cfpwdErrorMessage = "Not the Same Password";}
+    if(!this.emailError&&!isCorrectEmail){this.emailError=true; this.emailErrorMessage = "Not a correct Email";}
+   
+    if(this.userError)
+      this.username = ''; 
+    if(this.emailError)
+      this.email = '';    
+    if(this.pwdError)
+      this.password = '';  
+    if(this.cfpwdError)
+      this.confirmPwd = ''; 
+    
+    if (!this.userError&&!this.emailError&&!this.pwdError&&!this.cfpwdError){
+      this.UserDTO = <JSON><unknown>{
+        "username": this.username,
+        "email" : this.email,
+        "password": this.password
+      }          
+      console.log(this.UserDTO);
+      this.navigateToLogin();
+    }
   }
 
 
