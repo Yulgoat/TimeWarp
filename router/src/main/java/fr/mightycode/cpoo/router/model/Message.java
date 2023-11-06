c37139386b2a8f8af9f908e358b61265adc3753d
vbarat <vincent.barat@gmail.com>
2023-11-06 13:47:29 +0100

    Router app improvements imported from mvp branch.

--- router/src/main/java/fr/mightycode/cpoo/router/model/Message.java
+++ router/src/main/java/fr/mightycode/cpoo/router/model/Message.java
@@ -6,9 +6,10 @@ import java.util.UUID;
 
 @Data
 public class Message {
-  private UUID id;      // unique id of the message
-  private String from;  // sender address
-  private String to;    // recipient address
-  private String type;  // MIME type of the body
-  private String body;  // body (BASE64 encoded for binary types)
+  private UUID id;        // unique id of the message
+  private long timestamp; // timestamp of the message
+  private String from;    // sender address
+  private String to;      // recipient address
+  private String type;    // MIME type of the body
+  private String body;    // body (BASE64 encoded for binary types)
 }
